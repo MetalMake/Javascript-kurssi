a1fa3c067fb068bd304b011d4556a65a97ee032c
MetalMake <59830249+MetalMake@users.noreply.github.com>
2020-03-14 18:35:48 +0200

    Add files via upload

---
+++ Ws2index.js
@@ -0,0 +1,39 @@
+//Ex1.
+/* document.write
+("<i><strong>\"If I had nine hours to chop down a tree, I'd spend the first six sharpening my axe.\"</strong><br>-Abraham Lincoln</i>"); */
+
+//Ex2.
+/* function repeatingText(){
+for (let i = 0; i < 50; i++)
+document.write("Hello World!!!<br>");
+
+}
+repeatingText(); */
+
+//Ex.3
+/* if (navigator.appCodeName = "Firefox"){
+location.href = "http://www.mozilla.org"
+
+}
+else
+location.href = "http://www.microsoft.com"
+ */
+//Ex4.
+
+/* if (confirm("Where to go?")== true)
+console.log("You pressed OK");
+else
+console.log("You pressed cancel");
+ */
+
+ //Ex5.
+ /* let numero = Math.round((Math.random()*2));
+ let images =[
+    "http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg",
+    "http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg",
+    "http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg"
+
+ ];
+ document.write("<img src='https://cdn-images-1.medium.com/max/1600/1*ot7tWiPCYC01pV0kGmK3qQ.png'>");
+ document.images[0].src = images[numero];
+ */
